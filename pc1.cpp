903d8215a00c43a065beeb791349320ed1738791
Andelp <128000107+Andelp@users.noreply.github.com>
2024-04-04 07:27:26 -0500

    Update pc1.cpp

--- pc1.cpp
+++ pc1.cpp
@@ -4,48 +4,46 @@
 
 
 int main(){
-    std::string nombre1;
-    int edad1;
-    std::string nombre2;
-    int edad2;
+    std::string nombre;
+    int edad;
 
     student estudiante1("nr",2);
     student estudiante2("na",3);
 
     std::cout<<"ingrese su nombre: ";
-    getline(std::cin,nombre1);
+    getline(std::cin,nombre);
 
-    estudiante1.setname(nombre1);
+    estudiante1.setname(nombre);
 
     std::cout<<"ingrese su edad: ";
-    std::cin>>edad1;
+    std::cin>>edad;
 
-    estudiante1.setage(edad1);
-
+    estudiante1.setage(edad);
+    std::cin.ignore();
 
     std::cout<<"ingrese su nombre: ";
-    getline(std::cin,nombre2);
+    getline(std::cin,nombre);
 
-    estudiante2.setname(nombre2);
+    estudiante2.setname(nombre);
 
     std::cout<<"ingrese su edad: ";
-    std::cin>>edad2;
+    std::cin>>edad;
 
-    estudiante2.setage(edad2);
+    estudiante2.setage(edad);
 
     
 
 
 
 
-    std::cout<<estudiante1.getname()<<estudiante1.getage()<<std::endl;
+    std::cout<<estudiante1.getname()<<" "<<estudiante1.getage()<<std::endl;
     if(estudiante1.older()==0){
         std::cout<<"no es mayor de edad"<<std::endl;
     }
     else{
         std::cout<<"es mayor de edad"<<std::endl;
     }
-    std::cout<<estudiante2.getname()<<estudiante2.getage()<<std::endl;
+    std::cout<<estudiante2.getname()<<" "<<estudiante2.getage()<<std::endl;
     if(estudiante2.older()==0){
         std::cout<<"no es mayor de edad"<<std::endl;
     }

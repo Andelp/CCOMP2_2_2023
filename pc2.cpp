d275220979336b66477b2a5ca15247b4702abb4c
Andelp <128000107+Andelp@users.noreply.github.com>
2024-04-04 08:25:46 -0500

    Update pc2.cpp

--- pc2.cpp
+++ pc2.cpp
@@ -3,9 +3,27 @@
 using namespace std;
 
 int main() {
-  Date fecha1(2020,1,3);
-  Date fecha2(2019,14,15);
-  Date fecha3(1500,);
+  Date fecha1(0,0,0);
+  Date fecha2(0,0,0);
+  Date fecha3(0,0,0);
+
+  fecha1.setDay(3);
+  fecha1.setMonth(1);
+  fecha1.setYear(2019);
+
+  fecha2.setDay(15);
+  fecha2.setMonth(14);
+  fecha2.setYear(2019);
+
+  fecha3.setDay(20);
+  fecha3.setMonth(12);
+  fecha3.setYear(1500);
+
+  fecha1.displayDate();
+  fecha2.displayDate();
+  fecha3.displayDate();
+
+  
 
 
 }
